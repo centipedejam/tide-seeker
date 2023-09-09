7ba223971b2fe634fdbf54e830e3f32668623de8
centipedejam <jheimburge@gmail.com>
2023-09-08 20:38:17 -0700

    add barebones new spot functionality

--- controllers/spots.js
+++ controllers/spots.js
@@ -1,10 +1,15 @@
 const Spot = require('../models/spot');
 
 module.exports = {
-    index
+    index,
+    new: newSpot,
 }
 
 async function index (req, res) {
     const spots = await Spot.find({});
     res.render('spots/index', {title: 'Index Functionality', spots})
 };
+
+function newSpot (req, res) {
+    res.render('spots/new', {title: 'Add Surf Spot'})
+}
--- routes/spots.js
+++ routes/spots.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const spotsCtrl = require('../controllers/spots')
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 
 // All routes start with '/spots' 
 
 //GET '/spots' (index)
 router.get('/', spotsCtrl.index);
+//GET '/spots/new' (new)
+router.get('/new', ensureLoggedIn, spotsCtrl.new);
 
 
 
