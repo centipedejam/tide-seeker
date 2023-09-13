a94b12011f185480b39fdcd0ae06417bbe8785d8
centipedejam <jheimburge@gmail.com>
2023-09-13 15:29:59 -0700

    add styling to app

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([32.83, -117.27], 5);
+var map = L.map('map').setView([36.83, -120.27], 5.5);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -9,12 +9,9 @@ fetch('/api/surfspots')
     .then(surfSpotData => {
         surfSpotData.forEach(spot => {
             const latitude = parseFloat(spot.lat);
-            console.log(latitude)
             const longitude = parseFloat(spot.long);
-            console.log(longitude)
             const marker = L.marker([latitude, longitude]).addTo(map);
-
-            marker.bindPopup(`<a href="#">${spot.name}</a> <br><img class="marker-img" src="${spot.img}">`);
+            marker.bindPopup(`<a href="/spots/${spot._id}">${spot.name}</a> <br><img class="marker-img" src="${spot.img}">`);
         });
     })
     .catch(error => console.error(error));
