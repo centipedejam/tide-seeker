1f35b307e7da58a864a00afb05e6a2e1e4b90c9f
centipedejam <jheimburge@gmail.com>
2023-09-17 22:36:29 -0700

    dynamically render button for favorite spot

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -4,6 +4,7 @@ const spotId = addToFavoritesButton.getAttribute('spotId');
 
 addToFavoritesButton.addEventListener('click', (e) => {
   e.preventDefault();
+  addToFavoritesButton.style.visibility = 'hidden';
   axios.post(`/users/${userId}/favorites`, { spotId })
     .then(response => {
       alert('you liked this page!')
